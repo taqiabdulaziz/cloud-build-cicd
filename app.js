361c3e87130ad9228f80cf653882a0ce36c178d4
taqiabdulaziz <muhammad.taqi@myboost.id>
2019-07-31 23:56:03 +0700

    aa

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res) => {
-  res.send(config)
+  res.send(process.env)
 });
 app.use('/users', usersRouter);
 
